b53d51e3577780a9798aa5d2f8df190c4e3621b8
cegrief <cegrief@gmail.com>
2015-05-04 19:19:48 -0500

    super-janky google plus integration

---
+++ app/example/scripts/IndexController.js
@@ -0,0 +1,23 @@
+angular
+    .module('example')
+    .controller('IndexController', function($scope, supersonic) {
+
+        function signinCallback(authResult) {
+            alert(authResult);
+            if (authResult['status']['signed_in']) {
+                // Update the app to reflect a signed in user
+                // Hide the sign-in button now that the user is authorized, for example:
+
+
+                document.getElementById('signinButton').setAttribute('style', 'display: none');
+            } else {
+                // Update the app to reflect a signed out user
+                // Possible error values:
+                //   "user_signed_out" - User is signed-out
+                //   "access_denied" - User denied access to your app
+                //   "immediate_failed" - Could not automatically log in the user
+                supersonic.logger.log('Sign-in state: ' + authResult['error']);
+            }
+        }
+
+    });
