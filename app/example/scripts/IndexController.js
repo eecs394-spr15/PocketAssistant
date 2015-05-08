eae6413012b7e384cff2c2e68c4261066c414b59
mingyueding <mingyueding2015@u.northwestern.edu>
2015-05-07 21:47:57 -0500

    add icons; date works

--- app/example/scripts/IndexController.js
+++ app/example/scripts/IndexController.js
@@ -1,17 +1,17 @@
 angular
     .module('example')
     .controller('IndexController', function($scope, supersonic) {
-
-        today=new Date();
+        $scope.today=new Date();
         function initArray(){
             this.length=initArray.arguments.length;
             for(var i=0;i<this.length;i++)
                 this[i+1]=initArray.arguments[i]}
         var d=new initArray("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-        $scope.month = today.getMonth() + 1;
-        $scope.day = today.getDate();
-        $scope.year =today.getYear()-100;
-        $scope.xx = d[today.getDay()+1];
+        $scope.month_names=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        $scope.month = $scope.month_names[$scope.today.getMonth()];
+        $scope.date = $scope.today.getDate();
+        $scope.year =$scope.today.getFullYear();
+        $scope.day = d[$scope.today.getDay()+1];
 
         $scope.auth = function() {
             var config = {
@@ -44,4 +44,22 @@ angular
                 $scope.events = events;
             });
         }
+
+        /*$scope.sugg = [{activity:'gym',count:0},{activity:'meal',count:0},{activity:'pills',count:0}];*/
+        $scope.suggAct=['gym','meal','pills'];
+        $scope.suggCount = [0,0,0];
+        $scope.take = [false, false,false];
+        $scope.suggId = 0;
+        $scope.takeSugg= function() {
+            $scope.suggCount[$scope.suggId] += 1;
+            $scope.take[$scope.suggId] = true;
+        };
+        $scope.nextSugg = function() {
+            if ($scope.suggId < $scope.suggAct.length-1) {
+                $scope.suggId +=1;
+            }
+            else {
+                $scope.suggId = 0;
+            }
+        };
     });
