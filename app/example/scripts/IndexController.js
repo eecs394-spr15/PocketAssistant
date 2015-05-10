905a0eeebfffa2f713c717ad83ee74582fac282e
hebiao064 <hebiaobuaa@gmail.com>
2015-05-10 15:55:33 -0500

    Calendar works, but still dealing with the button issue

--- app/example/scripts/IndexController.js
+++ app/example/scripts/IndexController.js
@@ -13,19 +13,42 @@ angular
         $scope.year =today.getYear()-100;
         $scope.xx = d[today.getDay()+1];
 
-        $scope.auth = function() {
-            var config = {
-                'client_id': '792909163379-01odbc9kccakdhrhpgognar3d8idug0q.apps.googleusercontent.com',
-                'scope': 'https://www.googleapis.com/auth/calendar',
-                'immediate': 'true'
-            };
-            gapi.auth.authorize(config, function (tok) {
-                supersonic.logger.log('login complete');
-                supersonic.logger.log(tok);
-                $scope.token = gapi.auth.getToken();
-                gapi.client.load('calendar', 'v3', getCalendarData)
-            });
+        var clientId = '792909163379-01odbc9kccakdhrhpgognar3d8idug0q.apps.googleusercontent.com';
+        var scopes = 'https://www.googleapis.com/auth/calendar';
+        var apiKey = 'AIzaSyAZkvW_yVrdUVEjrO7_DwFq2NidEkSEAoE';
+
+        $scope.handleClientLoad = function() {
+            supersonic.logger.log('enter');
+            // Step 2: Reference the API key
+            gapi.client.setApiKey(apiKey);
+            window.setTimeout(checkAuth,1);
         };
+
+        function checkAuth() {
+            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+        }
+
+        function handleAuthResult(authResult) {
+            var authorizeButton = document.getElementById('authorize-button');
+            if (authResult && !authResult.error) {
+                authorizeButton.style.visibility = 'hidden';
+                makeApiCall();
+            } else {
+                authorizeButton.style.visibility = '';
+                authorizeButton.onclick = handleAuthClick;
+            }
+        }
+
+        function handleAuthClick(event) {
+            // Step 3: get authorization to use private data
+            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+            return false;
+        }
+
+        function makeApiCall() {
+            gapi.client.load('calendar', 'v3', getCalendarData);
+        }
+
 
         function getCalendarData(){
             $scope.cal = true;
