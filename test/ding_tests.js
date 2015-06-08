38d65cb3ab361a268633e928b98f8830a8e93597
mingyueding <mingyueding2015@u.northwestern.edu>
2015-06-07 20:19:21 -0500

    Ding's unit test

---
+++ test/ding_tests.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Mingyue on 6/7/15.
+ */
+describe("A suite",function(){
+
+    beforeEach(module('example'));
+
+    var controller, $scope;
+
+    beforeEach(inject(function($controller, $rootScope){
+        $scope = $rootScope.$new();
+        controller = $controller('IndexController',{
+            $scope:$scope
+        });
+    }));
+
+    it("contains spec with an expectation", function(){
+
+        $scope.isNotHourLong($scope.sugg[0]);
+        expect(true).toBe(true);
+    })
+});
