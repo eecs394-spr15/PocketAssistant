f2ad848cf4b9c4323fa56fd1fb84116024ebb514
mingyueding <mingyueding2015@u.northwestern.edu>
2015-06-09 02:18:46 -0500

    ding's test. also made some change on js file.

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [
     ],
-    
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
