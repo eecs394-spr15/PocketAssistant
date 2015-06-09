ffc3a7cfec283b46cc090099336608b98b203e26
mingyueding <mingyueding2015@u.northwestern.edu>
2015-06-09 02:15:49 -0500

    ding's test. also made some change on js file.

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,9 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -33,8 +31,7 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [
     ],
-
-
+    
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
